c0f5beee141dfc0d9984ee74fb2a67e0af5b90d4
tahhh <thadpongsermsook1@gmail.com>
2022-08-18 16:01:13 +0700

    completed

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,87 @@ import {
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const markTodo = (idx) => {};
+  useEffect(() => {
+    const todoStr = localStorage.getItem("react-todos-1");
+    if (todoStr === null) setTodos([]);
+    else setTodos(JSON.parse(todoStr));
+  }, []);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    saveTodos();
+  }, [todos]);
 
-  const moveUp = (idx) => {};
+  function onmouse() {
+    onmouseover = () => {
+      doneBtn.style.display = "";
+      deleteBtn.style.display = "";
+    };
 
-  const moveDown = (idx) => {};
+    onmouseout = () => {
+      doneBtn.style.display = "none";
+      deleteBtn.style.display = "none";
+    };
+  }
+
+  const deleteTodo = (idx) => {
+    todos.splice(idx, 1);
+    const newTodos = [...todos];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (idx) => {
+    todos[idx].completed = !todos[idx].completed;
+    setTodos([...todos]);
+  };
+
+  const moveUp = (idx) => {
+    if (idx === 0) return;
+    const title = todos[idx].title;
+    const completed = todos[idx].completed;
+
+    todos[idx].title = todos[idx - 1].title;
+    todos[idx].completed = todos[idx - 1].completed;
+
+    todos[idx - 1].title = title;
+    todos[idx - 1].completed = completed;
+    setTodos([...todos]);
+  };
+
+  const moveDown = (idx) => {
+    if (idx === todos.length - 1) return;
+    const title = todos[idx].title;
+    const completed = todos[idx].completed;
+
+    todos[idx].title = todos[idx + 1].title;
+    todos[idx].completed = todos[idx + 1].completed;
+
+    todos[idx + 1].title = title;
+    todos[idx + 1].completed = completed;
+    setTodos([...todos]);
+  };
+
+  const saveTodos = () => {
+    const todoStr = JSON.stringify(todos);
+    localStorage.setItem("react-todos-1", todoStr);
+  };
+
+  const onKeyUpHandler = (e) => {
+    if (e.key !== "Enter") return;
+    if (todoText == "") alert("Todo cannot be empty");
+    else {
+      const newTodos = [{ title: todoText, completed: false }, ...todos];
+      setTodos(newTodos);
+      setTodoText("");
+      saveTodos();
+    }
+  };
 
   return (
     <div>
@@ -28,40 +102,40 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onChange={(e) => {
+            setTodoText(e.target.value);
+          }}
+          value={todoText}
+          onKeyUp={onKeyUpHandler}
         />
         {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
-
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
+        <div>
+          {todos.map((todo, i) => (
+            <Todo
+              title={todo.title}
+              completed={todo.completed}
+              key={i}
+              onDelete={() => deleteTodo(i)}
+              onMark={() => markTodo(i)}
+              onMoveUp={() => moveUp(i)}
+              onMoveDown={() => moveDown(i)}
+            />
+          ))}
         </div>
 
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({todos.length}) </span>
+          <span className="text-warning">
+            Pending ({todos.filter((x) => x.completed === false).length})
+          </span>
+          <span className="text-success">
+            Completed ({todos.filter((x) => x.completed === true).length})
+          </span>
         </p>
-
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Thadpong Sermsook 640610636
         </p>
       </div>
     </div>
